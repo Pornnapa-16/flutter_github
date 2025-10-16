a97e1ead2642850375200bc98b754e596a02fa49
Pornnapa-16 <119863268+Pornnapa-16@users.noreply.github.com>
2025-10-16 11:41:05 +0700

    Update: แก้ไขข้อความเป็นชื่อตัวเอง

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('ฉันชื่อ.....'),
+          child: Text('โค้ดของพรนพา'),
         ),
       ),
     );
